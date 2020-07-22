0f6c43841a26074e7d3a8fd38c2629ad5d0308a4
KiranKumar Reddy Dumpa <passion4IAS@outlook.com>
2020-07-23 00:11:28 +0530

    Adding the sample code for Azure Application Insights which is an Spring boot api application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # azure-app-insights
 Azure Application Insights
+
+Source URL: https://github.com/lenisha/spring-demo-monitor.git
---
+++ src/main/java/com/microsoft/demoai/HelloController.java
@@ -0,0 +1,110 @@
+package com.microsoft.demoai;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.microsoft.applicationinsights.telemetry.Duration;
+import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import io.micrometer.core.annotation.Timed;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloController {
+    private final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    TelemetryClient telemetryClient;
+    private CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+
+    @GetMapping("/trackAuto")
+    public int trackDependencyAutomatically() throws IOException {
+        HttpGet httpGet = new HttpGet("https://www.google.com");
+        int status;
+        try (CloseableHttpResponse response = closeableHttpClient.execute(httpGet)) {
+            status = response.getStatusLine().getStatusCode();
+        }
+        return status;
+    }
+
+    @GetMapping("/track")
+    public String hellotrace() {
+
+        // track a custom event
+        //telemetryClient.trackEvent("Sending a custom event...");
+
+        // trace a custom trace
+        //telemetryClient.trackTrace("Sending a custom trace....");
+
+        double drand = new Random().nextDouble();
+        int rand = new Random().nextInt(10);
+        Map<String, String> props = new HashMap<String,String>();
+        props.put("team", "team" + rand);
+        props.put("client", "client" + rand);
+
+        // track a custom metric
+        telemetryClient.trackMetric("custom split metric", drand, 2, 0.0, 1.0, null, props);
+        
+        // track a custom dependency
+        telemetryClient.trackDependency("ClientProfile Service", "Get", new Duration(0, 0, 1, 1, 1), true);
+
+        return "hellotrace";
+    }
+
+    @GetMapping("/trackdim")
+    public String hellodimension() {
+        
+        int rand = new Random().nextInt(10);
+        // track custom dimension
+        RequestTelemetry telemetry = new RequestTelemetry();
+        telemetry.getProperties().put("team", "team" + rand);
+        telemetry.getProperties().put("client", "client" + rand);
+
+        telemetryClient.track(telemetry);
+
+        return "hello";
+    }
+
+    @GetMapping("/trackex")
+    public String trackexeption() {
+        HelloUtils.trackexception();
+        return "hello";
+    }
+
+    @GetMapping("/ex")
+    public String unhandled() {
+        throw new NullPointerException();
+    }
+
+     @GetMapping("/trackslow")
+     public String trackslow() {
+         HelloUtils.slow();
+         return "hello slow";
+     }
+
+     @GetMapping("/slow")
+     @Timed("slowcall")
+     public String slow() {
+         HelloUtils.slow();
+         return "hello slow";
+     }
+
+     @GetMapping("/hello")
+     public String hello() {
+        log.info("info logback hello");
+        return "hello";
+     }
+ }
